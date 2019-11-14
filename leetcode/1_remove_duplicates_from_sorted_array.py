733dc63ff52ede8865e31e93ac6af09a735e118a
Sandeep Giri <sandeep@cloudxlab.com>
2019-11-14 14:54:40 +0530

    moved to a folder

---
+++ leetcode/1_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,41 @@
+
+'''
+1.1 Remove Duplicates from Sorted Array
+
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+'''
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return len(nums)
+        i = 0
+        j = i + 1
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i+1
---
+++ leetcode/7_plus_one.py
@@ -0,0 +1,37 @@
+'''
+Plus One
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+'''
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i = len(digits) -1
+        while i > -1:
+            if digits[i] == 9:
+                digits[i] = 0
+                i -= 1
+            else:
+                digits[i] += 1
+                break
+        if i == -1:
+            digits.insert(0, 1)
+        return digits
